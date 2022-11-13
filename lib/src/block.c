9ae82470d6a76c513c7b576473267a7e007d34f1
Dragon Zap Education <daniel@dragonzap.com>
2022-11-13 16:43:06 +0200

    second bug fix

--- lib/src/block.c
+++ lib/src/block.c
@@ -414,8 +414,8 @@ struct block *block_clone(struct block *block)
     transaction_group->total_transactions = block->transaction_group->total_transactions;
     memcpy(transaction_group->hash, block->transaction_group->hash, sizeof(transaction_group->hash));
     struct block* block_cloned = block_create_with_group(block->hash, block->prev_hash, transaction_group);
-    block_cloned->key = *block->key;
-    block_cloned->signature = *block->signature;
+    block_cloned->key = block->key;
+    block_cloned->signature = block->signature;
     return block;
 }
 
