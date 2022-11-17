88c01a1a3b53f03d8f382ee400cccf6597924d2c
Dragon Zap Education <daniel@dragonzap.com>
2022-11-17 22:22:37 +0200

    commit

--- lib/src/block.c
+++ lib/src/block.c
@@ -220,9 +220,13 @@ BLOCKCHAIN_TYPE blockchain_should_create_new(struct block *block, int *blockchai
 
     // Lets get the block with the previous hash
     struct block* pervious_block = block_load(block->prev_hash);
+    if (!pervious_block)
+    {
+        magicnet_log("%s BUG\n", __FUNCTION__);
+        return MAGICNET_BLOCKCHAIN_TYPE_NO_NEW_CHAIN;
+    }
     *blockchain_id_out = pervious_block->blockchain_id;
     
-    // We can't make a chain right now we will try to make one later when we have more known blocks
     return MAGICNET_BLOCKCHAIN_TYPE_NO_NEW_CHAIN;
 }
 
--- lib/src/database.c
+++ lib/src/database.c
@@ -968,6 +968,7 @@ int magicnet_database_count_blocks_with_previous_hash(const char *prev_hash)
     res = sqlite3_prepare_v2(db, load_block_sql, strlen(load_block_sql), &stmt, 0);
     if (res != SQLITE_OK)
     {
+        res = -1;
         goto out;
     }
 
