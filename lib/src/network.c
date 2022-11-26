5c4974b3e06173c4d7baa5fe76cbfd9b562bf617
Dragon Zap Education <daniel@dragonzap.com>
2022-11-26 02:49:58 +0200

    commit

--- lib/src/network.c
+++ lib/src/network.c
@@ -3348,7 +3348,6 @@ void *magicnet_server_client_thread(void *_client)
     {
         // We must ask the server to relay packets to us
         res = magicnet_server_poll(client);
-        magicnet_log("test\n");
     }
     magicnet_server_lock(client->server);
     magicnet_close(client);
